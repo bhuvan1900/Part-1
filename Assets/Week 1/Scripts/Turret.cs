7df54907f605497d52747d27ab1d87485791ff35
Bhuvan Chalasani <chalasbh@shernet.sheridancollege.ca>
2024-01-11 21:19:45 -0500

    Task 4b completed- turrent rotation now possible

---
+++ Assets/Week 1/Scripts/Turret.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    float speed3 = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float keyboardInput = Input.GetAxis("Vertical");
+        
+        if (Input.GetKeyDown(KeyCode.V)) ;
+        {
+            transform.Rotate(keyboardInput * speed3 * Time.deltaTime * Vector3.forward, 1);
+        }
+    }
+
+    
+}
