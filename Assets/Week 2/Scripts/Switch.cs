06fe327b1fd760126dc02e9bdfc003b19d520b0d
Bhuvan Chalasani <chalasbh@shernet.sheridancollege.ca>
2024-01-18 11:06:46 -0500

    Week 2 Task2 complete- triggers now work

---
+++ Assets/Week 2/Scripts/Switch.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Color myColor;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = myColor;
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger from- " + collision.gameObject);
+        spriteRenderer.color = Color.red;
+        //gameObject.SetActive(false);
+        //spriteRenderer.enabled = false;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteRenderer.color = myColor;
+    }
+}
