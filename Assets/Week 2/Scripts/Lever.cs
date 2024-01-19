778ed7f29efcbebc8735db92a0f0f3aec3c847e3
Bhuvan Chalasani <chalasbh@shernet.sheridancollege.ca>
2024-01-18 23:34:55 -0500

    task2b not completed

---
+++ Assets/Week 2/Scripts/Lever.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject lever;
+    public GameObject door;
+    public Rigidbody2D hisRigidBody;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+    }
+}
